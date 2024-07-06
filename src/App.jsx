bc8da8e1f8d98d78f8fb930e5069f026194a192f
Ashish Kumar <sayhitoak@gmail.com>
2024-07-06 17:22:31 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css';
+import { HelloWorld } from './sections/hello-world';
+function App() {
+
+  return (
+    <>
+      <div>
+        <HelloWorld />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/hello-world/hello-world.jsx
@@ -0,0 +1,42 @@
+import styles from './hello-world.module.css';
+function HelloWorld() {
+
+    return (
+        <section className={styles["hello-world"] + ' flex justify-center content-center'}>
+            <svg width="809" height="305" viewBox="0 0 809 305" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path className="h1" d="M107 22V116" stroke="#999999" strokeWidth="40" strokeLinecap="round" />
+                <path className="h2" d="M107.5 69L177.5 69" stroke="#555555" strokeWidth="40" strokeLinecap="round" />
+                <path className="h3" d="M177.5 22V116" stroke="#111111" strokeWidth="40" strokeLinecap="round" />
+                <path className="h4" d="M108 69.5V68.5" stroke="#111111" strokeWidth="40" strokeLinecap="round" />
+                <path className="e1" d="M225.5 22V116" stroke="#555555" strokeWidth="40" strokeLinecap="round" />
+                <path className="e2" d="M226.5 69H262.5" stroke="#555555" strokeWidth="40" strokeLinecap="round" />
+                <path className="e3" d="M226 22L296 22" stroke="#999999" strokeWidth="40" strokeLinecap="round" />
+                <path className="e4" d="M226 116L296 116" stroke="#111111" strokeWidth="40" strokeLinecap="round" />
+                <path className="l1" d="M344.5 22V116" stroke="#111111" strokeWidth="40" strokeLinecap="round" />
+                <path className="l2" d="M345 116L415 116" stroke="#555555" strokeWidth="40" strokeLinecap="round" />
+                <path className="l3" d="M471 21V115" stroke="#777777" strokeWidth="40" strokeLinecap="round" />
+                <path className="l4" d="M471.5 115L541.5 115" stroke="#333333" strokeWidth="40" strokeLinecap="round" />
+                <circle className="o1" cx="631" cy="68" r="48" stroke="#333333" strokeWidth="40" />
+                <path className="w1" d="M25 182L52 276" stroke="#999999" strokeWidth="40" strokeLinecap="round" />
+                <path className="w2" d="M155 182L128 276" stroke="#999999" strokeWidth="40" strokeLinecap="round" />
+                <path className="w3" d="M90 208L51.9999 275.615" stroke="#555555" strokeWidth="40" strokeLinecap="round" />
+                <path className="w4" d="M90 208L128 275.615" stroke="#555555" strokeWidth="40" strokeLinecap="round" />
+                <circle className="o2" cx="251" cy="228" r="48" stroke="#333333" strokeWidth="40" />
+                <path className="r1" d="M361 234L431.5 275" stroke="#333333" strokeWidth="40" strokeLinecap="round" />
+                <path className="r2" d="M361 181V275" stroke="#777777" strokeWidth="40" strokeLinecap="round" />
+                <circle className="r3" cx="395" cy="214" r="34" stroke="#999999" strokeWidth="40" />
+                <path className="l6" d="M500 181V275" stroke="#777777" strokeWidth="40" strokeLinecap="round" />
+                <path className="l5" d="M500.5 275L570.5 275" stroke="#333333" strokeWidth="40" strokeLinecap="round" />
+                <circle className="d1" cx="669" cy="228" r="48" stroke="#333333" strokeWidth="40" />
+                <path className="d2" d="M619 181V275" stroke="#777777" strokeWidth="40" strokeLinecap="round" />
+                <path className="d3" d="M619 276V275" stroke="#999999" strokeWidth="40" strokeLinecap="round" />
+
+                <path className="ex1" d="M780 182V232" stroke="#777777" strokeWidth="40" strokeLinecap="round" />
+
+                <path className="ex2" d="M780.5 276L779.793 276.707" stroke="#333333" strokeWidth="40" strokeLinecap="round" />
+            </svg>
+        </section>
+    )
+}
+
+export default HelloWorld
