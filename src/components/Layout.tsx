1b0d75bd6f82b2a0fb127c16a828ca00172046ae
Ashish Kumar <sayhitoak@gmail.com>
2025-12-25 03:07:26 +0530

    Added tabs for hobbies

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import Navigation from "./Navigation";
 import { useLocation } from "react-router-dom";
 import { ROUTES } from "../constants";
@@ -18,7 +18,12 @@ function Layout({ children }: LayoutProps) {
   const backgroundClass =
     ROUTES[parsedActiveRoute]?.meta.backgroundClass ||
     ROUTES.WHOAMI.meta.backgroundClass;
-
+  const tabs = [
+    { id: 1, label: "Tab 1" },
+    { id: 2, label: "Tab 2" },
+    { id: 3, label: "Tab 3" },
+  ];
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
   return (
     <div
       className={classNames(
@@ -26,7 +31,7 @@ function Layout({ children }: LayoutProps) {
         backgroundClass
       )}
     >
-      <div className="w-5/6 flex items-end h-[75%]">
+      <div className="w-5/6 flex items-end h-[75%] relative">
         <svg width="63" height="44" viewBox="0 0 63 44" fill="none">
           <path d="M63 0V44H0L63 0Z" fill="black" />
         </svg>
@@ -36,9 +41,34 @@ function Layout({ children }: LayoutProps) {
           initial={{ height: "0" }}
           animate={{ height: "100%" }}
           transition={{ duration: 1, ease: [0.16, 1, 0.3, 1] }}
-          className="content bg-white bg-[radial-gradient(circle,#e0e0e0_1px,transparent_1px)] bg-size-[20px_20px] w-4/6  rounded-t-xl pt-8 p-6 pb-0 grow h-full border-2"
+          className="content bg-white bg-[radial-gradient(circle,#e0e0e0_1px,transparent_1px)] bg-size-[20px_20px] w-4/6  rounded-t-xl pt-8 p-6 pb-0 grow h-full  relative"
         >
-          <main className="flex-1 h-full">
+          <motion.div
+            initial={{ opacity: "0" }}
+            animate={{ opacity: "100%" }}
+            transition={{ duration: 0, ease: [0.16, 1, 0.3, 1], delay: 0.2 }}
+            className="flex gap-1 absolute rotate-90 right-[-115px] transform-[translate(50%,-100%)] top-0"
+            key={activeRoute}
+          >
+            {tabs.map((item) => (
+              <motion.button
+                key={item.id}
+                initial={{ opacity: 0.75, y: 4 }}
+                whileHover={{ y: 0 }}
+                whileTap={{ opacity: 1 }}
+                transition={{ type: "spring", stiffness: 300, damping: 20 }}
+                className={classNames(
+                  "bg-white p-2 pl-6 pr-6 rounded-t-sm cursor-pointer",
+                  activeTab === item.id &&
+                    "opacity-100! transform-[translateY(0)]! "
+                )}
+                onClick={() => setActiveTab(item.id)}
+              >
+                {item.label}
+              </motion.button>
+            ))}
+          </motion.div>
+          <main className="flex-1 h-full relative">
             <motion.div
               key={activeRoute}
               initial={{ opacity: 0, filter: "blur(10px)" }}
@@ -50,6 +80,7 @@ function Layout({ children }: LayoutProps) {
             </motion.div>
           </main>
         </motion.div>
+
         <svg width="63" height="44" viewBox="0 0 63 44" fill="none">
           <path d="M5.32134e-07 0V44H63L5.32134e-07 0Z" fill="black" />
         </svg>
